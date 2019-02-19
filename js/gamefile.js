f2327af52d47d289d20845d5dc639a7021e54da6
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2019-02-19 12:51:01 -0500

    Added a bit more

--- js/gamefile.js
+++ js/gamefile.js
@@ -8,6 +8,9 @@ NOTES:
       are high in war.
 
 
+TODO: I forgot to build in a foundation for win conditions... that was stupid.
+      Gotta add some stuff for that.
+
  *************/
 
 var record = {win: 0, loss: 0};
@@ -25,6 +28,19 @@ function imagepath(cardname)
 function int_val(cardname)
 {
     return parseInt(cardname.slice(0, cardname.length - 1));
+}
+
+// This is a (modern) Fisher-Yates shuffle:
+// More: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+function shuffle(cards)    
+{
+    let divis, holder
+    for (var i = cards.length - 1; i > 0; i--){
+	divis = Math.floor(Math.random() * (i + 1));
+	holder = cards[i];
+	cards[i] = cards[divis];
+	cards[divis] = holder;
+    }
 }
 
 /*
@@ -68,7 +84,7 @@ function compare(card1, card2, player1, player2)
 
 function turncards(player1, player2, list, count)
 {
-    for (i = 0; i < count; i++){
+    for (var i = 0; i < count; i++){
 	list.p1.unshift(player1.deck.pop());
 	list.p2.unshift(player2.deck.pop());
     }
@@ -78,17 +94,36 @@ function turncards(player1, player2, list, count)
 
 function givecards(player, list)
 {
+    let all_cards = [list.p1, list.p2];
+    for (var i = 0; i < 2; i++){
+	shuffle(all_cards[i]);
+	for (var l = 0; l < all_cards[i].length; l++){
+	    player.deck.push(all_cards[i][l])
+	}
+    }
 }
-    
+
+function tie(player1, player2, card_list)
+{
+    // TODO: Put one card face down
+    turncards(player1, player2, card_list, 1);
+    let winner = compare(turned_cards.p1[0], turned_cards.p2[0]);
+
+    if (winner === "TIE"){
+	return tie(player1, player2, card_list);
+    }else{
+	return winner;
+    }
+}   
 
 function turn(player1, player2)
 {
-    var turned_cards = {p1: [], p2: []};
+    let turned_cards = {p1: [], p2: []};
     turncards(player1, player2, turned_cards, 1);
-    var winner = compare(turned_cards.p1[0], turned_cards.p2[0]);
+    let winner = compare(turned_cards.p1[0], turned_cards.p2[0]);
 
     if (winner === "TIE"){
-	// TODO: insert tie handler
+	givecards(tie(player1, player2, turned_cards), turned_cards);
     }else{
 	givecards(winner, turned_cards);
     }

23bf4d149f73be1092815d6988dea6c75e79b705
Nate-Weeks <nweeks87@gmail.com>
2019-02-24 19:20:49 -0500

    reset

--- animate.js
+++ animate.js
@@ -2,15 +2,29 @@ var playerCard = document.getElementsByClassName('player-card')[0]
 var opponentCard = document.getElementsByClassName('opponent-card')[0]
 var vs = document.getElementsByClassName('vs')[0]
 
+function reset(){
+  setTimeout(function () {
+      playerCard.classList.remove('loseTranslate');
+      opponentCard.classList.remove('winTranslate');
+      vs.innerHTML = "play";
+      vs.style.visibility = "visible"
+    }, 3000)
+}
+
 vs.onclick = function() {
+  
   if(this.innerHTML === 'player won')
   {
     playerCard.classList.add('winTranslate');
     opponentCard.classList.add('loseTranslate');
-    vs.outerHTML = ""
+    vs.innerHTML = "";
+    vs.style.visibility = "hidden";
+    reset()
   } else if (this.innerHTML === 'opponent won') {
     playerCard.classList.add('loseTranslate');
     opponentCard.classList.add('winTranslate');
-    vs.outerHTML = ""
+    vs.innerHTML = "";
+    vs.style.visibility = "hidden";
+    reset()
   }
  }

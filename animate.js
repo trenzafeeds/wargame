02bc1efc437192d1df481bfc362dda3ab4e86fac
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2019-02-24 20:27:39 -0500

    starting to animate

--- animate.js
+++ animate.js
@@ -1,6 +1,9 @@
 var playerCard = document.getElementsByClassName('player-card')[0]
 var opponentCard = document.getElementsByClassName('opponent-card')[0]
 var vs = document.getElementsByClassName('vs')[0]
+var players = init_game();
+var player = players[0];
+var opponent = players[1];
 
 function reset(){
   setTimeout(function () {
@@ -12,19 +15,22 @@ function reset(){
 }
 
 vs.onclick = function() {
-  
-  if(this.innerHTML === 'player won')
-  {
-    playerCard.classList.add('winTranslate');
-    opponentCard.classList.add('loseTranslate');
-    vs.innerHTML = "";
-    vs.style.visibility = "hidden";
-    reset()
-  } else if (this.innerHTML === 'opponent won') {
-    playerCard.classList.add('loseTranslate');
-    opponentCard.classList.add('winTranslate');
-    vs.innerHTML = "";
-    vs.style.visibility = "hidden";
-    reset()
-  }
- }
+    let ret = turn(player, opponent);
+    if (ret[0] == 0){
+	if (ret[1] == 1){
+	    vs.innerHTML === 'player won';
+	    playerCard.classList.add('winTranslate');
+	    opponentCard.classList.add('loseTranslate');
+	    vs.innerHTML = "";
+	    vs.style.visibility = "hidden";
+	    reset()
+	}else if (ret[1] == 2){
+	    vs.innerHTML === 'opponent won'
+	    playerCard.classList.add('loseTranslate');
+	    opponentCard.classList.add('winTranslate');
+	    vs.innerHTML = "";
+	    vs.style.visibility = "hidden";
+	    reset()
+	}
+    }
+}

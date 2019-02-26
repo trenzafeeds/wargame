448303ec48246f15ece05d58640ba81574d63ed9
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2019-02-26 15:01:45 -0500

    removing final debugs

--- js/gamefile.js
+++ js/gamefile.js
@@ -288,9 +288,9 @@ function tie(player1, player2, card_list)
     showCards(card_list, w = true);
     let winner = compare(card_list.p1[0], card_list.p2[0]);
 
-    console.log(winner)
+    // console.log(winner)
     if (winner[0] == 3){
-	console.log("second tie!")
+	// console.log("second tie!")
 	return tie(player1, player2, card_list);
     }else{
 	return [0, winner];
